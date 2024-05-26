5567b667d5d5f14c9cfd850cdaf16fb55390bd06
Kanji Miyatani <kanji09012458@gmail.com>
2024-05-26 23:52:51 +0900

    resolve alias recognition issue in VSCode

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "../slices/counterSlice";
-import authSlice from "../slices/authSlice";
+import counterSlice from "@/slices/counterSlice";
+import authSlice from "@/slices/authSlice";
 
 export const store = configureStore({
     reducer: {
